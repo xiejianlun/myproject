af38061347237bc0e3d3b6dc9e45b690ff84a161
xiejianlun <541534224@qq.com>
2019-06-25 11:00:39 +0800

    test

--- rabbitmq/src/main/java/com/rabbitmq/RabbitmqApplication.java
+++ rabbitmq/src/main/java/com/rabbitmq/RabbitmqApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RabbitmqApplication {
 
+	//测试
 	public static void main(String[] args) {
+		System.out.print("---");
 		SpringApplication.run(RabbitmqApplication.class, args);
 	}
 
